5bee97c34fd813b387d499471fd0a0bdfabe7e97
Mario Mainz <mario.mainz@invision.de>
2020-03-13 10:58:39 +0100

    Implement first version of Valued module

--- lib/valued.rb
+++ lib/valued.rb
@@ -1,6 +1,57 @@
 require 'valued/version'
 
 module Valued
-  class Error < StandardError; end
-  # Your code goes here...
+  module ClassMethods
+    def attributes(*attributes)
+      attributes.each { |attr| attr_reader attr }
+      define_method('_attributes') { attributes }
+    end
+  end
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def initialize(attributes = {})
+    _attributes.each do |attribute|
+      if attributes.key?(attribute)
+        instance_variable_set(
+          "@#{attribute}",
+          attributes.fetch(attribute).freeze
+        )
+      end
+    end
+  end
+
+  def ==(other)
+    _attributes.all? do |attribute|
+      other.respond_to?(attribute) && send(attribute) == other.send(attribute)
+    end
+  end
+
+  def eql?(other)
+    self.class == other.class && self == other
+  end
+
+  def hash
+    (_attributes.map { |attribute| send(attribute) } + [self.class]).hash
+  end
+
+  def to_h
+    _attributes.each_with_object({}) do |attribute, hash|
+      hash[attribute] = send(attribute)
+    end
+  end
+
+  def to_s
+    inspect
+  end
+
+  def inspect
+    inspected_attributes =
+      _attributes.map do |attribute|
+        "#{attribute}=#{send(attribute).inspect}"
+      end.join(' ')
+    "#<#{self.class} #{inspected_attributes}>"
+  end
 end
--- spec/valued_spec.rb
+++ spec/valued_spec.rb
@@ -1,9 +1,91 @@
 RSpec.describe Valued do
-  it 'has a version number' do
-    expect(Valued::VERSION).not_to be nil
-  end
+  context 'defining a quantity value object' do
+    let(:quantity_class) do
+      Class.new do
+        include Valued
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+        attributes :unit, :amount
+      end
+    end
+    let(:instance) { quantity_class.new(unit: 'm', amount: 2) }
+    let(:duck_type) { Struct.new(:unit, :amount) }
+    let(:duck_object) { duck_type.new('m', 2) }
+
+    it 'defines an initializer and sets defined attributes' do
+      expect(instance.amount).to eq(2)
+      expect(instance.unit).to eq('m')
+    end
+
+    it 'can be initialized without params' do
+      empty_instance = quantity_class.new
+      expect(empty_instance.unit).to eq(nil)
+      expect(empty_instance.amount).to eq(nil)
+    end
+
+    it 'sets a value to nil if omitted in construction' do
+      quantity = quantity_class.new(amount: 1)
+
+      expect(quantity.unit).to eq(nil)
+    end
+
+    it 'does not allow to set attributes' do
+      expect { instance.amount = 3 }.to raise_error(NoMethodError)
+    end
+
+    it 'does not allow mutation of values' do
+      expect { instance.unit.upcase! }.to raise_error(FrozenError)
+    end
+
+    it 'does not allow to set arbitrary attributes on construction' do
+      expect(instance.instance_variable_get('@moep')).to be_nil
+      expect { instance.moep }.to raise_error(NoMethodError)
+    end
+
+    it 'equals another instance when values are equal' do
+      instance_one = quantity_class.new(unit: 'm', amount: 2)
+      instance_two = quantity_class.new(unit: 'm', amount: 2)
+      instance_three = quantity_class.new(unit: 'm', amount: 3)
+
+      expect(instance_one == instance_two).to eq(true)
+      expect(instance_two == instance_three).to eq(false)
+      expect(instance_one == instance_three).to eq(false)
+    end
+
+    it 'equals another object when the duck type matches' do
+      expect(instance == duck_object).to eq(true)
+    end
+
+    it 'does not raise when comparing to an arbitrary type' do
+      expect { instance == 1 }.to_not raise_error
+    end
+
+    it 'can be converted to a hash' do
+      expect(instance.to_h).to eq({ unit: 'm', amount: 2 })
+    end
+
+    it 'can construct itself with the hash it converts to' do
+      expect(quantity_class.new(instance.to_h)).to eq(instance)
+    end
+
+    it 'can be used as a hash key' do
+      hash = { quantity_class.new(instance.to_h) => 1 }
+
+      expect(hash[instance]).to eq(1)
+    end
+
+    it 'only equals a hash key if the class is the same' do
+      hash = { quantity_class.new(instance.to_h) => 1, duck_object => 2 }
+
+      expect(hash[instance]).to eq(1)
+    end
+
+    it 'implements to_s with inspect' do
+      expect(instance.to_s).to eq(instance.inspect)
+    end
+
+    it 'prints a proper inspect output' do
+      allow(instance).to receive(:class).and_return('Quantity')
+      expect(instance.inspect).to eq('#<Quantity unit="m" amount=2>')
+    end
   end
 end
